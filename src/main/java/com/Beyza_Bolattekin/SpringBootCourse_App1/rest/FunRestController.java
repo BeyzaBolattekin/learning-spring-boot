e3ef41a1622d2ca3db19d5dc11e5a67bd19e9814
BeyzaBolattekin <beyzabolattekin19@gmail.com>
2026-09-25 13:08:20 +0300

    injecting properties

--- src/main/java/com/Beyza_Bolattekin/SpringBootCourse_App1/rest/FunRestController.java
+++ src/main/java/com/Beyza_Bolattekin/SpringBootCourse_App1/rest/FunRestController.java
@@ -1,6 +1,7 @@
 package com.Beyza_Bolattekin.SpringBootCourse_App1.rest;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -8,6 +9,17 @@ public class FunRestController {
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @GetMapping("/appInfo")
+    public String getAppInfo() {
+        return "App Name is " + appName + "and version is " + appVersion;
     }
 
 }
